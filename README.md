1a6f33854702be4417a0702e6e3aa4e41559303a
Truman Smith <trumanwsmith@gmail.com>
2018-10-08 23:10:57 -0700

    add fun fact reveal. refactor for unobtrusive JS

--- README.md
+++ README.md
@@ -1,20 +1,38 @@
 
-HTML manipulation done using jQueryy
+HTML manipulation done using jQuery
+Unobtrusive JavaScript / Progressive Enhancement
+
+When JS is disabled the basic functionality of the page is still present
+and JS dependent elements are hidden.
+
+Elements dependent in JS are created in script tags in index.html.
 
 List of Top Restaurants section
-- Add an item to the list
-- Double-clicking on list item toggles between normal and strikeout with gray
+- Add an item to the list.
+- New items are flagged as New using css with content property.
+- Double-clicking on list item toggles between normal and grayed strikeout
 using jQuery.toggleClass() method.
 - Question-mark-in-circle icon from Google fonts
 - Help message appears when mouse hovers over "help text"
 
-Recent Reviews section
-- Use jQuery to show and change HTML on existing review.
+Fun Facts
+- Functionally, facts are hidden by a cover. Clicking the cover removes it and
+displays the fact. Implemented by showing and hiding the cover and fun fact
+elements at page load and clicking on cover.
 
 Add A Review section
 - Message appears for 2 seconds using slide down, slide up animation
 - Inputs in this section are displayed in a second review in Recent Reviews
 section
 
-TO-DO
-- Add reveal boxes as demonstrated in movie spoiler exercise
+
+FUNCTIONALITY WITHOUT JAVASCRIPT ENABLED
+- Top restaurants list appears
+- Answer to How Ranked question is shown.
+- Fun facts are not hidden.
+- Recent Reviews show existing review.
+
+Element that are not present
+- Inputs to add new restaurant.
+- Fun fact reveal buttons.
+- Add A Review inputs and submit.
--- js/app.js
+++ js/app.js
@@ -1,42 +1,35 @@
-//  FOR RESTAURANT List// 1. Hide/show the text inside the #helpText span element when the user's mouse passes over the text "How are these ranked?" (Hint: use the jQuery toggle() method)
-$('#helpText').on('mouseenter mouseleave', function(event) {$('#helpText span').toggle();});
 
-// 2. Append a new restaurant to the restaurant list when the user enters a value into the #newRestaurant input and clicks on the #addNew button.
+// Create listener for submit new restaurant
 $('#addNew').on('click', function() {
   let newRestaurant = $('#newRestaurantInput').val();
   if ( newRestaurant.length ) {
-    let newLI = $(`<li>${newRestaurant}</li>`);
+    let newLI = $(`<li class='new'>${newRestaurant}</li>`);
     $('ul').append(newLI);
     $('#newRestaurantInput').val('');
   }
 });
 
-  // BONUS: Before appending the new restaurant to the list, use string concatentation to make sure the new restaurant name is surrounded by an opening and closing <li> tag.
+// Hide the text inside the #helpText span element.
+// Create listener to show when the user's mouse passes over "How are these ranked?" text
+$('#helpText span').hide();
+$('#helpText').on('mouseenter mouseleave', function(event) {$('#helpText span').toggle();});
 
-// 3. Add the class "strike" to the restaurant name each time it's double clicked. Use event delegation on the ul element to listen for a click event on the ul's list items
+// Create listener on restaurant list to toggle between "strike" and regular font on double click.
 $('ul').on('dblclick', 'li', function(event) {
   $(this).toggleClass('strike');
 });
 
-//BONUS: Refactor challenge 3 using the "this" keyword.
-//BONUS: Look up a jQuery method that will let you toggle the .strike class on and off.
+// Hide the fun facts and show their cover element
+$('.funfact').hide();
+$('.fact-cover').show();
 
+// Create listener to show fun fact and hide cover
+$('.fact-cover').on('click', function(event) {
+  $(this).prev().show();
+  $(this).hide();
+});
 
-//  FOR REVIEW SECTIONS
-
-// 1. Show the hidden restaurant review, or use an animation method to reveal it.
-$('div.review').show();
-
-// 2. Set the title of the hidden restaurant review to a different name -- whatever you'd like.
-$('div.review h3').text('Dat Dar Dog');
-
-// 3. Change the text of the hidden restaurant review. The review should contain HTML, such as a strong tag or a span element.
-$('div.review p').html('The <strong>best</strong> gator sausage New Orleans has to offer! <i>Yes!</i>');
-
-// 4. Change the name of the app. Change *only* the text inside the span element within the h1.
-$('h1 span').text('Yell!');
-
-// 5. Create an animation for the paragraph with the id of "flashMessage." Chain an animation that
+// Wait for a moment, then hide flash message
 $('#flashMessage')
   // slides the message down for 1 second
   .slideDown(1000)
@@ -45,7 +38,8 @@ $('#flashMessage')
   // slides the message up for 1 second
   .slideUp(1000);
 
-// 6. Inside the click handler, create a working restaurant review form.
+
+// Create listener for submit review
 $('#submitBtn').click(function(){
   //  a. Use the val() method to get what's been entered into the restaurant name and restaurant review form input fields. Save both values to variables.
   //  b. Insert the restaurant name value into the #newRestaurant h3 tag
